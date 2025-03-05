814af333370deaef957065269de1b3a32a17902d
ahmedhabeeb73 <alnaib888@gmail.com>
2025-03-05 12:35:01 +0100

    add login and register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
+use App\Http\Requests\ApiLoginReqeust;
 
 class AuthController extends Controller
 {
     use ApiResponses;
     //
-    public function login()
+    public function login(ApiLoginReqeust $request)
     {
-        return $this->successResponse('Login Success');
+        return $this->successResponse($request->get('email'));
+    }
+
+    public function register(Request $request)
+    {
+        return $this->successResponse($request->all());
     }
 }
